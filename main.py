d0054ded5ed4920f4a8d806a66b4f4a61ed5b64e
Super-DLT <learndlt5@gmail.com>
2021-05-16 12:31:32 +0430

    more math

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Welcome Message
 print("Welcome to tip calculator :)\n")
 # Get the total Bill
-bill = input("What was the total bill? $\n")
+bill = float(input("What was the total bill? $"))
 # Get the persentage 
-persentage = input("How persentage tip would you like to give ? 10, 12 or 15\n")
+persentage = int(input("How persentage tip would you like to give ? 10, 12 or 15\n"))
 # Get the amount of people that want to tip
-people = input("How many people to split the tip ?\n")
+people = int(input("How many people to split the bill ?\n"))
 # Doing some Math 
-total_tip = ((int(persentage) * float(bill)) / 100) / int(people)
+total_bill = (persentage / 100 * bill + bill) / people
 # Print the result
-print(f"Each Person Should Pay ${total_tip}")
+print(f"Each Person Should Pay ${round(total_bill, 2)}")
 
